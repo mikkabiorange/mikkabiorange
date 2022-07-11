5552f11db453811881a80bf0290a3b675d1aebe5
mikkabiorange <100075452+mikkabiorange@users.noreply.github.com>
2022-07-11 23:37:01 +0900

    Add files via upload

---
+++ LINE.py
@@ -0,0 +1,52 @@
+token="4z5ABFP4UWBFxEoLaU3ZvT4V5fkVLxx5vuSGUsvZKSM"
+url="https://notify-api.line.me/api/notify"
+from bs4 import BeautifulSoup
+import requests
+
+tenki_url="https://weather.yahoo.co.jp/weather/jp/22/5040.html"
+
+#　天気取得
+def Weather(AreaCode):
+    url = "https://weather.yahoo.co.jp/weather/jp/21/" + str(AreaCode) + ".html"
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    rs = soup.find(class_='forecastCity')
+    rs = [i.strip() for i in rs.text.splitlines()]
+    rs = [i for i in rs if i != ""]
+    print(rs[0] + "の天気は" + rs[1] + "、明日の天気は" + rs[19] + "です。")
+    return "岐阜 "+str(rs[0]) + "の天気は" + str(rs[1]) + "、明日の天気は" + str(rs[2]) + "です。"
+
+#　天気取得
+def Weather2(AreaCode):
+    url = "https://weather.yahoo.co.jp/weather/jp/33/" + str(AreaCode) + ".html"
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    rs = soup.find(class_='forecastCity')
+    rs = [i.strip() for i in rs.text.splitlines()]
+    rs = [i for i in rs if i != ""]
+    print(rs[0] + "の天気は" + rs[1] + "、明日の天気は" + rs[19] + "です。")
+    return "岡山 "+str(rs[0]) + "の天気は" + str(rs[1]) + "、明日の天気は" + str(rs[2]) + "です。"
+
+message=str(Weather(5210))
+
+message=message+"\r\n"+str(Weather2(6610))
+
+# 名言取得
+def saying_text():
+    url = 'http://www.meigensyu.com/quotations/view/random'
+    res = requests.get(url)
+    html = res.text
+    soup = BeautifulSoup(html, 'html.parser')
+    saying = soup.find('div',class_='text')
+    saying = [i.strip() for i in saying.text.splitlines()]
+    saying = [i for i in saying if i != ""]
+    return saying
+
+qwe2=str(saying_text())
+
+auth={"Authorization":"Bearer "+token}
+content={"message":message+"\r\n""今日の名言"+"\r\n"+qwe2}
+
+requests.post(url, headers=auth, data=content)
+
+
